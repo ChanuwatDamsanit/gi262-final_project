0bfa3ce6d8e5f58073d612b77ba8954a648a4643
Chanuwat-Damsanit <chanuwatrs@gmail.com>
2025-11-06 17:13:52 +0700

    Add KeyGiver NPC

--- Assets/Workshop/Student/Scripts/OOP/OOPMapGenerator.cs
+++ Assets/Workshop/Student/Scripts/OOP/OOPMapGenerator.cs
@@ -17,6 +17,7 @@ namespace Solution
         [Header("Set NPC")]
         public NPC Npc;
         public NPC Healer;
+        public NPC KeyGiver;
         public NPCSkill NpcSkill;
         [Header("Set Exit")]
         public OOPExit Exit;
@@ -119,12 +120,15 @@ namespace Solution
         {
             mapdata[Npc.positionX, Npc.positionY] = Npc;
             mapdata[Healer.positionX, Healer.positionY] = Healer;
+            mapdata[KeyGiver.positionX, KeyGiver.positionY] = KeyGiver;
             mapdata[NpcSkill.positionX, NpcSkill.positionY] = NpcSkill;
             Npc.transform.position = new Vector3(Npc.positionX, Npc.positionY, 0);
             Healer.transform.position = new Vector3(Healer.positionX, Healer.positionY, 0);
+            KeyGiver.transform.position = new Vector3(KeyGiver.positionX, KeyGiver.positionY, 0);
             NpcSkill.transform.position = new Vector3(NpcSkill.positionX, NpcSkill.positionY, 0);
             Npc.mapGenerator = this;
             Healer.mapGenerator = this;
+            KeyGiver.mapGenerator = this;
             NpcSkill.mapGenerator = this;
         }
         private void SetUpExit()
---
+++ Assets/Workshop/Student/Scripts/Tree/KeyGiverDialogue.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using Solution;
+
+public class KeyGiverDialogue : DialogueSequen
+{
+    [Header("References")]
+    public Character player; // Assign your player (must have Inventory component)
+    private Inventory playerInventory; // cache inventory
+
+    [Header("Key Info")]
+    public string keyName = "Key";
+    public int keyAmount = 2;
+
+    protected override void LoadConversations()
+    {
+        // Dialogue nodes
+        DialogueNode greeting = new DialogueNode("Hey there, traveler. What do you want?");
+        DialogueNode firstAsk = new DialogueNode("You want a key, huh? Say that again, I didn’t hear you.");
+        DialogueNode deny = new DialogueNode("Then you don’t need a key, I guess.");
+        DialogueNode secondAsk = new DialogueNode("Fine, here’s your key. Don’t lose it!");
+        DialogueNode alreadyHave = new DialogueNode("You already have a key. Don’t be greedy!");
+        DialogueNode goodbye = new DialogueNode("Alright, see you around.");
+
+        // --- Build tree ---
+        greeting.AddNext(firstAsk, "I want a key");
+        greeting.AddNext(deny, "Nothing, just looking around.");
+
+        firstAsk.AddNext(secondAsk, "I want a key");
+        firstAsk.AddNext(deny, "Nevermind.");
+
+        tree = new DialogueTree(greeting);
+    }
+
+    public override void SelectChoice(int index)
+    {
+        base.SelectChoice(index);
+
+        // Cache inventory if not already done
+        if (player != null && playerInventory == null)
+        {
+            playerInventory = player.GetComponent<Inventory>();
+        }
+
+        // Check if this is the key-giving node
+        if (currentNode != null && currentNode.text == "Fine, here’s your key. Don’t lose it!")
+        {
+            if (playerInventory != null)
+            {
+                // Only give if player doesn’t already have it
+                if (!playerInventory.HasItem(keyName, keyAmount))
+                {
+                    playerInventory.AddItem(keyName, keyAmount);
+                    Debug.Log($"{player.name} received {keyAmount}x {keyName}!");
+                }
+                else
+                {
+                    // Replace dialogue text to show "already have" message
+                    currentNode = new DialogueNode("You already have a key. Don’t be greedy!");
+                    dialogueUI.ShowDialogue(currentNode);
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Player has no Inventory component!");
+            }
+        }
+    }
+}
